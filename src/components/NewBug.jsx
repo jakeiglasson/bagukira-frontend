730865f68d7a4f1f3c55bdb8f51417f8f8eee528
Jake Glasson <jakeiglasson@gmail.com>
2020-07-21 11:18:12 +1000

    added staticc new bug submition form

--- src/components/NewBug.jsx
+++ src/components/NewBug.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import "./css/NewBug.css";
+import "./css/Global.css";
 
 class NewBug extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="side-content-container p-4">
+        <div className="p-4 global-form-container">
+          <h2 className="text-center">NEW BUG FORM</h2>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>REPORTER NAME</Form.Label>
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>{" "}
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>BUG SUBJECT</Form.Label>
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>BUG DESCRIPTION</Form.Label>
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    );
   }
 }
 
