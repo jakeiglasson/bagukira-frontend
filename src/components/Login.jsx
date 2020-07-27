4a5daa512bc38fca9397bf62ea8055e444275dc0
Jake Glasson <jakeiglasson@gmail.com>
2020-07-27 16:07:21 +1000

    removed comments from Login.jsx, modified logic in Projects.jsx to retrieve projects from backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,9 +18,6 @@ class Login extends Component {
   }
 
   handleSubmit = () => {
-    // UserId needs to be dynamically set when user login functionality is working. Setting as a placeholder for now
-    // localStorage.setItem("userId", 1);
-    // localStorage.setItem("userEmail", "jake@gmail.com");
     let { email, password } = this.state;
     axios
       .post(process.env.REACT_APP_API_URL + "/login", {
@@ -78,9 +75,7 @@ class Login extends Component {
               onChange={this.onInputChange}
             />
           </Form.Group>
-          {/* <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group> */}
+
           <Link
             to="/projects"
             className="text-link"
@@ -102,73 +97,3 @@ class Login extends Component {
 }
 
 export default Login;
-
-// class Login extends React.Component {
-//   state = { email: "", password: "", errMessage: "" };
-
-//   onInputChange = (event) => {
-//     const key = event.target.id;
-//     this.setState({
-//       [key]: event.target.value,
-//     });
-//   };
-
-//   onFormSubmit = async (event) => {
-//     event.preventDefault();
-//     const { email, password } = this.state;
-//     const body = {
-//       auth: { email, password },
-//     };
-//     try {
-//       const response = await fetch("http://localhost:3000/login", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(body),
-//       });
-//       if (response.status >= 400) {
-//         throw new Error("incorrect credentials");
-//       } else {
-//         const { jwt } = await response.json();
-//         localStorage.setItem("token", jwt);
-//         this.props.history.push("/secrets");
-//       }
-//     } catch (err) {
-//       this.setState({
-//         errMessage: err.message,
-//       });
-//     }
-//   };
-
-//   render() {
-//     const { email, password, errMessage } = this.state;
-//     return (
-//       <div className="container">
-//         <h1>Login</h1>
-//         {errMessage && <span>{errMessage}</span>}
-//         <form onSubmit={this.onFormSubmit}>
-//           <label htmlFor="email">Email</label>
-//           <input
-//             type="email"
-//             name="email"
-//             id="email"
-//             value={email}
-//             onChange={this.onInputChange}
-//           />
-//           <label htmlFor="password">Password</label>
-//           <input
-//             type="password"
-//             name="password"
-//             id="password"
-//             value={password}
-//             onChange={this.onInputChange}
-//           />
-//           <input type="submit" value="Submit" />
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Login;
